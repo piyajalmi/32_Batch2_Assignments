e4380ea097c77d919d0fffe7242dc45cd5801fba
piya-jalmi <143387306+piyajalmi@users.noreply.github.com>
2024-10-13 16:14:44 +0530

    Add files via upload

---
+++ 32_Batch2_LegislativeAssembly/src/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GoaLegislativeAssembly","l":"GoaLegislativeAssembly()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"GoaLegislativeAssembly","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
